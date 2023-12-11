80717d93a52c6bb8c9422e132d2e5862f8ab7612
0xcronos <0xcronos@gmail.com>
2023-12-11 16:18:33 +0000

    feature: Adds usb devices to config and removes unused module

--- brain/src/rpi-camera-integration-test.py
+++
@@ -1,8 +0,0 @@
-from envyaml import EnvYAML
-from object_detector.recyclable_object_detector import RecyclableObjectDetector
-
-config = EnvYAML('config.yaml')	
-object_detector = RecyclableObjectDetector(config['object_detector'])
-print("Object detector created successfully...")
-
-object_detector.execute()
